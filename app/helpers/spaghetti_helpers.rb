570102679f771eb9359207673dbf570c94e2b7c3
Sanav <sana181019811@gmail.com>
2013-07-09 03:38:02 +0500

    added option for starting page

--- app/helpers/spaghetti_helpers.rb
+++ app/helpers/spaghetti_helpers.rb
@@ -1,8 +1,9 @@
 module SpaghettiHelpers
   def spaghetti name, path_or_items, options = {}
-    output = ''.html_safe
+    output    = ''.html_safe
+    next_page = next_page_id(options)
     if path_or_items.class.name == 'String'
-      output << "<div class=\"#{ name } spaghetti spaghetti-empty\" data-spaghetti-enabled=\"1\" data-spaghetti-url=\"#{ path_or_items }\" data-spaghetti-next-page=\"1\"><div class=\"#{ name }-items\"></div><div class=\"throbber\">#{ image_tag 'throbber.gif' }</div></div>".html_safe
+      output << "<div class=\"#{ name } spaghetti spaghetti-empty\" data-spaghetti-enabled=\"1\" data-spaghetti-url=\"#{ path_or_items }\" data-spaghetti-next-page=\"#{next_page}\"><div class=\"#{ name }-items\"></div><div class=\"throbber\">#{ image_tag 'throbber.gif' }</div></div>".html_safe
     else
       if path_or_items.any?
         if options[:partial]
@@ -11,7 +12,7 @@ module SpaghettiHelpers
           items = j(render(path_or_items))
         end
           output << "$('.#{ name } .#{ name }-items').append('#{ items }');".html_safe
-        output << "$('.#{ name }').data('spaghetti-next-page', #{ params[:page].to_i + 1 });".html_safe
+        output << "$('.#{ name }').data('spaghetti-next-page', #{ next_page });".html_safe
         output << "$('.#{ name }').data('spaghetti-enabled', '1');".html_safe
       else
         output << "$('.#{ name }').data('spaghetti-enabled', '0');".html_safe
@@ -20,5 +21,14 @@ module SpaghettiHelpers
       output << "$('.#{ name }').removeClass('spaghetti-loading');".html_safe
     end
     output
-  end  
+  end
+
+  protected
+  def next_page_id(options = {})
+    if params[:page]
+      params[:page].to_i + 1
+    else
+      options[:starting_page] || 1
+    end
+  end
 end
